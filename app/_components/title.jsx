80f70f1f38335e757bd78f54fed106343730885d
Mart510 <martynandrewbillings@gmail.com>
2023-11-21 21:15:11 +0000

    set up titler compoenet

--- app/_components/title.jsx
+++ app/_components/title.jsx
@@ -9,10 +9,32 @@ export default function Titler() {
     return (
         <>
             <div id='titleBox'>
-                <h1 className="">Martyn</h1>
-                <h1>Billings</h1>
+                <h1 className='font-sans text-8xl font-bold text-left pr-44'>Martyn</h1>
+                <h1 className="font-sans text-8xl font-bold text-right pl-44
+                 bg-pink-400 mt-2">Billings</h1>
+                <div className="font-sans text-4xl text-justify flex justify-between mt-1">
+                <p>F</p>
+                <p>u</p>
+                <p>l</p>
+                <p>l</p>
+                <p> </p>
+                <p>S</p>
+                <p>t</p>
+                <p>a</p>
+                <p>c</p>
+                <p>k</p>
+                <p> </p>
+                <p>D</p>
+                <p>e</p>
+                <p>v</p>
+                <p>e</p>
+                <p>l</p>
+                <p>o</p>
+                <p>p</p>
+                <p>e</p>
+                <p>r</p>
+                </div>
             </div>
-                <h2>Full Stack Developer</h2>
         </>
     )
 }
--- app/page.js
+++ app/page.js
@@ -1,5 +1,10 @@
 import Image from 'next/image'
 
+// google font import
+import { Quicksand } from 'next/font/google'
+
+const quicksand = Quicksand({ subsets: ['latin'], variable: '--font-quicksand', });
+
 // component imports
 import Titler from './_components/title'
 
@@ -7,8 +12,8 @@ export default function Home() {
   return (
     <>
     <nav>Nav</nav>
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    < Titler />
+    <main className={`${quicksand.variable} font-sans flex min-h-screen flex-col items-center justify-between p-24`}>
+      < Titler />
     </main>
     <footer>footer</footer>
     </>
