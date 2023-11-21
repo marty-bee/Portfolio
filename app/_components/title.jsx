9b8a37da4bb8a52a55872f97506e95ed5a2d3f0b
Mart510 <martynandrewbillings@gmail.com>
2023-11-21 20:38:03 +0000

    starting titler

---
+++ app/_components/title.jsx
@@ -0,0 +1,18 @@
+// import block
+
+
+// This component shows the main title on the home page of the portofolio
+
+export default function Titler() {
+
+
+    return (
+        <>
+            <div id='titleBox'>
+                <h1 className="">Martyn</h1>
+                <h1>Billings</h1>
+            </div>
+                <h2>Full Stack Developer</h2>
+        </>
+    )
+}
