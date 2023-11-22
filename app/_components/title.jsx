f7372591f97ca4451e63d7ef58e34c922980d2c1
Mart510 <martynandrewbillings@gmail.com>
2023-11-22 09:53:45 +0000

    added accessibility to full stack devbar

--- app/_components/title.jsx
+++ app/_components/title.jsx
@@ -12,28 +12,28 @@ export default function Titler() {
                     <h1 className='font-sans text-8xl font-bold text-left pr-4'>Martyn</h1>
                     <h1 className="font-sans text-8xl font-bold text-right pl-4
                     bg-pink-400 mt-2">Billings</h1>
-                    <div className="font-sans text-4xl text-justify flex justify-between mt-1">
+                    <div className="font-sans text-4xl text-justify flex justify-between mt-1" aria-label="Full Stack Developer">
                     {/* This is to do even spacing across the width of the div, it's less clunky than doing a text spacing value */}
-                    <p>F</p>
-                    <p>u</p>
-                    <p>l</p>
-                    <p>l</p>
-                    <p> </p>
-                    <p>S</p>
-                    <p>t</p>
-                    <p>a</p>
-                    <p>c</p>
-                    <p>k</p>
-                    <p> </p>
-                    <p>D</p>
-                    <p>e</p>
-                    <p>v</p>
-                    <p>e</p>
-                    <p>l</p>
-                    <p>o</p>
-                    <p>p</p>
-                    <p>e</p>
-                    <p>r</p>
+                    <p aria-hidden='true'>F</p>
+                    <p aria-hidden='true'>u</p>
+                    <p aria-hidden='true'>l</p>
+                    <p aria-hidden='true'>l</p>
+                    <p aria-hidden='true'> </p>
+                    <p aria-hidden='true'>S</p>
+                    <p aria-hidden='true'>t</p>
+                    <p aria-hidden='true'>a</p>
+                    <p aria-hidden='true'>c</p>
+                    <p aria-hidden='true'>k</p>
+                    <p aria-hidden='true'> </p>
+                    <p aria-hidden='true'>D</p>
+                    <p aria-hidden='true'>e</p>
+                    <p aria-hidden='true'>v</p>
+                    <p aria-hidden='true'>e</p>
+                    <p aria-hidden='true'>l</p>
+                    <p aria-hidden='true'>o</p>
+                    <p aria-hidden='true'>p</p>
+                    <p aria-hidden='true'>e</p>
+                    <p aria-hidden='true'>r</p>
                     </div>
                 </div>
             </div>
