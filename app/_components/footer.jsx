af8f1836ca1510de4a5947be07e44a7db7be72de
Mart510 <martynandrewbillings@gmail.com>
2023-11-22 08:48:42 +0000

    component progress

---
+++ app/_components/footer.jsx
@@ -0,0 +1,12 @@
+// import block
+
+// this component contains all the links at the bottom of the page
+
+export default function Footer() {
+    //
+
+    // return block
+    return (
+        <h1 className="text-4xl font-sans font-bold text-center mt-44">Get in touch</h1>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -6,8 +6,9 @@ import { Quicksand } from 'next/font/google'
 const quicksand = Quicksand({ subsets: ['latin'], variable: '--font-quicksand', });
 
 // component imports
-import Titler from './_components/title'
+import Titler from './_components/title';
 import Navbar from './_components/navbar';
+import Footer from './_components/footer';
 
 export default function Home() {
   return (
@@ -15,8 +16,11 @@ export default function Home() {
     <main className={`${quicksand.variable} font-sans`}>
     < Navbar />
     < Titler />
+    {/* < ProjectBox /> */}
+    {/* < Toolbox /> */}
+    {/* < AboutMe /> */}
+    < Footer />
     </main>
-    <footer>footer</footer>
     </>
   )
 }
