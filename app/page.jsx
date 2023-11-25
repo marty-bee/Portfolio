6a11bc585ff4c61ec4d8a9fe9551083e604f7680
Mart510 <martynandrewbillings@gmail.com>
2023-11-25 13:29:05 +0000

    set up new branch to do mobile styling

--- app/page.jsx
+++ app/page.jsx
@@ -9,10 +9,12 @@ import GetInTouch from './_components/GetInTouch';
 import ProjectBox from './_components/projectBox';
 import Toolbox from './_components/toolbox';
 import AboutMe from './_components/aboutMe';
+import Footer from './_components/footer';
 
 // data import
 import { projectsData } from "./projectData/projectCardData.js";    // array of project objects
 import { toolBoxData } from './projectData/toolBoxData' // array of tools
+import { imageOptimizer } from 'next/dist/server/image-optimizer';
 
 // home function is called 
 export default function Home() {
@@ -25,6 +27,7 @@ export default function Home() {
     < Toolbox />
     < AboutMe />
     < GetInTouch />
+    < Footer />
     </main>
     </>
   )
