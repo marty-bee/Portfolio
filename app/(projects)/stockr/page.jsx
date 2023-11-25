804c611c4a4db6046f73e74eb7a81c89ba7f2050
Mart510 <martynandrewbillings@gmail.com>
2023-11-25 20:37:50 +0000

    setting up for new pages

---
+++ app/(projects)/stockr/page.jsx
@@ -0,0 +1,12 @@
+// import block
+// google font import
+import { Quicksand } from 'next/font/google'
+const quicksand = Quicksand({ subsets: ['latin'], variable: '--font-quicksand', });
+
+
+export default function Stockr(){
+
+    return(
+        <p>test</p>
+    )
+}
