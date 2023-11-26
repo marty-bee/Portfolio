b0967d2b0c3860f41387ce6d963bd2f8232bf06a
Mart510 <martynandrewbillings@gmail.com>
2023-11-26 16:57:43 +0000

    buttons added and animations

---
+++ app/_components/_project-page-comonents/projectTechStack.jsx
@@ -0,0 +1,12 @@
+// import block
+
+
+// component to render buttons with link(s) to the projects gitHub repo and live deployment if applicable
+export default function ProjectTechStack({techStackArray}) {
+    // destructure button array
+
+    // return block
+    return (
+        null // for testing
+    )
+    }
---
+++ app/projects/stockr/page.jsx
@@ -0,0 +1,13 @@
+// import block
+import ProjectPage from "@/app/_components/_project-page-comonents/projectPage"
+
+export default function Stockr(){
+
+    return(
+        <>
+            < ProjectPage projectTitle={'Stockr'}
+            buttonLinks={{gitHubLink: 'dave', liveLink: 'dave'}}
+            techStack={null} />
+        </>
+    )
+}
