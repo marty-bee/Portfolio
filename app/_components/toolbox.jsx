d48bcb29c09aa0fcef760e22d2f8f6352f9951fb
Mart510 <martynandrewbillings@gmail.com>
2023-11-26 09:11:51 +0000

    scale change on hover added to elements

--- app/_components/toolbox.jsx
+++ app/_components/toolbox.jsx
@@ -14,33 +14,33 @@ export default function Toolbox(){
         {/* defining the styling and the grid layout rows alternate between 4 and 5 items starting at row 1 with 4 */}
 
             <div id='row1' className="flex justify-between w-full text-center p-1 sm:p-3  text-sm sm:text-xl font-sans font-bold dark:text-zinc-950">
-                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-pink-300 rounded-full' >JavaScript</p>
-                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-pink-300 rounded-full' >HTML</p>
-                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-pink-300 rounded-full' >CSS</p>
-                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-pink-300 rounded-full' >Node</p>
-                <p className='p-2 sm:p-3 sn:w-48 h-auto bg-pink-300 rounded-full' >TypeScript</p>
+                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-pink-300 rounded-full hover:scale-105' >JavaScript</p>
+                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-pink-300 rounded-full hover:scale-105' >HTML</p>
+                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-pink-300 rounded-full hover:scale-105' >CSS</p>
+                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-pink-300 rounded-full hover:scale-105' >Node</p>
+                <p className='p-2 sm:p-3 sn:w-48 h-auto bg-pink-300 rounded-full hover:scale-105' >TypeScript</p>
             </div>
 
             <div id='row2' className="flex justify-between w-full text-center p-1 sm:p-3 pl-4 pr-4 sm:pl-24 sm:pr-24 text-sm sm:text-xl font-sans font-bold dark:text-zinc-950">
-                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-blue-300 rounded-full' >React</p>
-                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-blue-300 rounded-full' >NextJS</p>
-                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-blue-300 rounded-full' >Tailwind</p>
-                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-blue-300 rounded-full' >PostgreSQL</p>
+                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-blue-300 rounded-full hover:scale-105' >React</p>
+                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-blue-300 rounded-full hover:scale-105' >NextJS</p>
+                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-blue-300 rounded-full hover:scale-105' >Tailwind</p>
+                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-blue-300 rounded-full hover:scale-105' >PostgreSQL</p>
             </div>
 
             <div id='row3' className="flex justify-between w-full text-center p-1 sm:p-3 text-sm sm:text-xl font-sans font-bold dark:text-zinc-950">
-                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-orange-300 rounded-full' >SQL</p>
-                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-orange-300 rounded-full' >Netlify</p>
-                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-orange-300 rounded-full' >PlayWright</p>
-                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-orange-300 rounded-full' >Vitest</p>
-                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-orange-300 rounded-full' >REST API</p>
+                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-orange-300 rounded-full hover:scale-105' >SQL</p>
+                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-orange-300 rounded-full hover:scale-105' >Netlify</p>
+                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-orange-300 rounded-full hover:scale-105' >PlayWright</p>
+                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-orange-300 rounded-full hover:scale-105' >Vitest</p>
+                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-orange-300 rounded-full hover:scale-105' >REST API</p>
             </div>
 
             <div id='row4' className="flex justify-between w-full text-center p-1 sm:p-3 pl-4 pr-4 sm:pl-24 sm:pr-24 text-sm sm:text-xl font-sans font-bold dark:text-zinc-950">
-                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-yellow-400 rounded-full' >Figma</p>
-                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-yellow-400 rounded-full' >Git</p>
-                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-yellow-400 rounded-full' >GitHub</p>
-                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-yellow-400 rounded-full' >Express</p>
+                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-yellow-400 rounded-full hover:scale-105' >Figma</p>
+                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-yellow-400 rounded-full hover:scale-105' >Git</p>
+                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-yellow-400 rounded-full hover:scale-105' >GitHub</p>
+                <p className='p-2 sm:p-3 sm:w-48 h-auto bg-yellow-400 rounded-full hover:scale-105' >Express</p>
             </div>
         </div>
     </>
