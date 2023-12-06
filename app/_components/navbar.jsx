fd961d45728c71ed120f64df6012377e38541f1f
Mart510 <martynandrewbillings@gmail.com>
2023-12-06 19:42:15 +0000

    basic navbar implemented

--- app/_components/navbar.jsx
+++ app/_components/navbar.jsx
@@ -1,6 +1,8 @@
 'use client'
 // import block
 import { useScroller, scroller } from '../_customHooks/useScroll'
+import Image from "next/image"
+import Link from "next/link"
 
 // This component contains the navigation bar and all the links as part of it
 export default function Navbar() {
@@ -23,15 +25,21 @@ export default function Navbar() {
     // // return
     // function to for the toggle switch
 
+
+
+
     return (
         <>
             <nav className="flex font-sans text-right p-4 md:text-xl w-full justify-between sm:justify-end">
-                <button className="font-sans sm:pr-20">Work</button>
-                <button className="font-sans sm:pr-20">About</button>
-                <button className="font-sans sm:pr-20">Contact</button>
-                <button className="font-sans sm:pr-20">CV</button>
+                <Link href={'/'} className="font-sans sm:mr-auto text-center">Home</Link>
+                {/* <button className="font-sans pl-2 pr-2 sm:mr-20">Work</button> */}
+                {/* <button className="font-sans pl-2 pr-2 sm:mr-20">About</button> */}
+                {/* <button className="font-sans pl-2 pr-2 sm:mr-20">Contact</button> */}
+                {/* <button className="font-sans pl-2 pr-2 sm:mr-20">CV</button> */}
+                <Link href={'https://github.com/Mart510'} className="flex items-center pl-2 pr-2 font-sans sm:mr-20">GitHub</Link>
+                <Link href={'https://www.linkedin.com/in/martbillings/'} className="flex items-center pl-2 pr-2 font-sans">LinkedIn</Link>
                 {/* button to toggle dark/light theme */}
-                <button className='font-sans sm:pr-4'>Theme</button>
+                {/* <button className='font-sans sm:pr-4'>Theme</button> */}
             </nav>
         </>
     )
