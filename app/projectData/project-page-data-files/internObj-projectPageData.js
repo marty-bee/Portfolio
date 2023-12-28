37eadb21c8fa4b4695abe4d69c7d31c01c5b813a
Mart510 <martynandrewbillings@gmail.com>
2023-12-28 12:20:16 +0000

    copy implemented, working on images now

---
+++ app/projectData/project-page-data-files/internObj-projectPageData.js
@@ -0,0 +1,45 @@
+// this file contains example objects that need to be populated for each project page, duplicate and rename this file for each project and adjust contents to needs/
+export const portfolioProjObj = {
+  // projectTitle - used to render the project title
+  projectTitle: "Intern",
+  // buttonLinks - used to render the link buttons
+  buttonLinks: {
+    gitHubLink: "https://github.com/Christener19/intern",
+    liveLink: 'https://intern-soc.vercel.app/',
+  },
+  // techStack - used to render the tech stack pills
+  techStack: [
+    {
+      colorCode: "pink",
+      techname: "TypeScript",
+    },
+    {
+      colorCode: "blue",
+      techname: "Node",
+    },
+    {
+      colorCode: "orange",
+      techname: "NextJs",
+    },
+    {
+      colorCode: "yellow",
+      techname: "Tailwind",
+    },
+    {
+      colorCode: "pink",
+      techname: "GitHub",
+    },
+    {
+      colorCode: "blue",
+      techname: "Vercel",
+    },
+    {
+      colorCode: "orange",
+      techname: "React",
+    },
+    {
+      colorCode: "yellow",
+      techname: "PostgreSQL",
+    },
+  ],
+};
