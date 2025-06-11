647e750276977373f87bb921827c6bce5fb8caab
Mart510 <martynandrewbillings@gmail.com>
2025-06-11 20:50:49 +0100

    dep update

--- app/_components/navbar.jsx
+++ app/_components/navbar.jsx
@@ -9,10 +9,6 @@ export default function Navbar({ initialTheme }) {
             <nav className="flex font-sans text-right p-4 md:text-xl w-full justify-between sm:justify-end">
 
                 <Link href={'/'} className="font-sans sm:mr-auto text-center hover:scale-105">mart.codes</Link>
-                {/* <button className="font-sans pl-2 pr-2 sm:mr-20 hover:scale-105">Work</button> */}
-                {/* <button className="font-sans pl-2 pr-2 sm:mr-20 hover:scale-105">About</button> */}
-                {/* <button className="font-sans pl-2 pr-2 sm:mr-20 hover:scale-105">Contact</button> */}
-                {/* <button className="font-sans pl-2 pr-2 sm:mr-20 hover:scale-105">CV</button> */}
                 <Link href={'https://github.com/marty-bee'} className="flex items-center pl-2 pr-2 font-sans sm:mr-20 hover:scale-105">GitHub</Link>
                 <Link href={'https://www.linkedin.com/in/martbillings/'} className="flex items-center pl-2 pr-2 
                 sm:mr-20
